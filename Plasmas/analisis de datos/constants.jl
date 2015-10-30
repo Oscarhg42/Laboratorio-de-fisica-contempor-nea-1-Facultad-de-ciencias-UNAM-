e6df7f2f5ca988523980ca7056d28c3553b62387
Oscarhg42 <oscar_hg@ciencias.unam.mx>
2015-10-29 23:07:34 -0600

    Versión preeeliminar (contiene algunos detalles que se deben corregir)

---
+++ Plasmas/analisis de datos/constants.jl
@@ -0,0 +1,11 @@
+module constants
+export h_planck,k_boltz,e_electron,m_electron,m_proton,Navog
+
+h_planck=6.62606957e-34 #J s
+k_boltz=1.3806488e-23 #J Kelvin
+e_electron = 1.602176565e-19 #Coulomb
+m_electron= 9.10938291e-31 #Kg
+m_proton=1.672621777e-27 #Kilogram
+Navog=6.02214129e23 # particulas por Mol
+eVolt=1.602176565e-19 #Joules
+end
